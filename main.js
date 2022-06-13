f9f8f0d70cabe65b414f17242a33b4bf664603ca
Kat-Tinka <karolinathorwarth@web.de>
2022-06-13 15:33:46 +0200

    Test

---
+++ main.js
@@ -0,0 +1,10 @@
+/*Exercise 1
+In your JavaScript file create a variable called myName with your name as the value. Put your name inside string quotes, e.g., "my name". Then add a line of code to print the variable name to the console after the previous message.
+
+Save your JavaScript file in your editor. Reload the HTML page in your browser. You should see your name printed. If not, investigate and fix it.*/
+
+let myName = ("Karolina"   "Thorwarth");
+
+
+console.log(myName);
+
